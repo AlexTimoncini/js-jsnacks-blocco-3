cb8a2e9f6e837caa8e4d88f6566fd09321051de5
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-15 12:53:11 +0200

    added header component

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -5,9 +5,18 @@
 <template>
     <header>
         <h1>Main Logo</h1>
+        <nav>
+            <ul v-for="n in 4">
+                <li>Home</li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
-    
+    @use '../styles/partials/mixins.scss' as *;
+    @use '../styles/partials/variables.scss' as *;
+    header{
+        background-color: $primaryColor;
+    }
 </style>
